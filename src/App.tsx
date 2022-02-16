c2752b1d6149bf18a450667df1eeb6287590fa08
Pawel <pawel-czarnecki.o2.pl>
2022-02-16 23:53:50 +0000

    modified sytles for main App coompenent and added new componenet 'InputField' and styles to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import InputField from './components/InputField';
 import './App.css';
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<span className="heading">Taskify</span>
+			<InputField />
 		</div>
 	);
 };
---
+++ src/components/InputField.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const InputField = () => {
+	return (
+		<form className="input">
+			<input className="input__box" type="text" placeholder="Enter a task" />
+			<button className="input__submit" type="submit">
+				Add
+			</button>
+		</form>
+	);
+};
+
+export default InputField;
